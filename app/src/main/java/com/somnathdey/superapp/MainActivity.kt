b8ca678e47c8bc4897ef3478bc75f3481bfedd05
Somnath Dey <somnath.dey@aprodence.com>
2025-05-15 23:56:20 +0530

    Initial commit

---
+++ app/src/main/java/com/somnathdey/superapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.somnathdey.superapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.somnathdey.superapp.ui.theme.SuperAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperAppTheme {
+
+            }
+        }
+    }
+}
